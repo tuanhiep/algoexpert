7ab6206dd182b71ce6d4a2c9a9b4b80a161686f2
bigname <tuanhiep.tran88@gmail.com>
2020-08-18 11:05:57 -0600

    solve problem of 2 numbers sum using smarter way of initializing array

--- src/main/java/two_number_sum.java
+++ src/main/java/two_number_sum.java
@@ -3,36 +3,24 @@ import java.util.*;
 class Program {
     public static int[] twoNumberSum1(int[] array, int targetSum) {
         // Write your code here.
-        int[] result = new int[2];
         for (int i : array) {
             for (int j : array) {
-
                 if (i != j && i + j == targetSum) {
-
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
+                    return new int[]{i, j};
                 }
             }
-
         }
         return new int[0];
     }
 
     public static int[] twoNumberSum2(int[] array, int targetSum) {
         // Write your code here.
-        int[] result = new int[2];
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
-
                 if (array[i] + array[j] == targetSum) {
-
-                    result[0] = array[i];
-                    result[1] = array[j];
-                    return result;
+                    return new int[]{array[i], array[j]};
                 }
             }
-
         }
         return new int[0];
     }
@@ -41,15 +29,11 @@ class Program {
         // Write your code here.
         HashMap<Integer, Integer> map = new HashMap();
         for (int i : array) {
-
             map.put(i, targetSum - i);
             if (targetSum - i != i && map.containsKey(targetSum - i)) {
-
                 return new int[]{i, targetSum - i};
             }
         }
-
         return new int[0];
     }
 }
-
