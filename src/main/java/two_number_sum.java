728a144eba1ef8e7507b0ce0e8235e1a2e990d66
bigname <tuanhiep.tran88@gmail.com>
2020-08-18 10:44:14 -0600

    solve problem of 2 numbers sum solution 2

--- src/main/java/two_number_sum.java
+++ src/main/java/two_number_sum.java
@@ -1,16 +1,34 @@
 import java.util.*;
 
 class Program {
-    public static int[] twoNumberSum(int[] array, int targetSum) {
+    public static int[] twoNumberSum1(int[] array, int targetSum) {
         // Write your code here.
-        int[] result= new int[2];
-        for(int i : array){
-            for(int j : array){
+        int[] result = new int[2];
+        for (int i : array) {
+            for (int j : array) {
 
-                if (i!=j && i+j==targetSum){
+                if (i != j && i + j == targetSum) {
 
-                    result[0]=i;
-                    result[1]=j;
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+
+        }
+        return new int[0];
+    }
+
+    public static int[] twoNumberSum2(int[] array, int targetSum) {
+        // Write your code here.
+        int[] result = new int[2];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+
+                if (array[i] + array[j] == targetSum) {
+
+                    result[0] = array[i];
+                    result[1] = array[j];
                     return result;
                 }
             }
