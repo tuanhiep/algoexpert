3339f295ca1c3bcc37a7d3c3c1de742817ff396f
bigname <tuanhiep.tran88@gmail.com>
2020-08-20 19:18:14 -0600

    solve problem of finding closest value in Binary Search Tree

---
+++ src/main/java/Problem3_ClosestBST.java
@@ -0,0 +1,48 @@
+import java.util.HashSet;
+
+public class Problem3_ClosestBST {
+    public static int findClosestValueInBst(BST tree, int target) {
+
+        HashSet<BST> traversed = new HashSet<BST>();
+        BST p = tree;
+
+        while (p != null) {
+            traversed.add(p);
+            if (p.value > target) {
+                p = p.left;
+            } else {
+                p = p.right;
+            }
+        }
+
+        // check which location
+        int min = getDistance(tree.value, target);
+        BST lo = tree;
+        for (BST b : traversed) {
+            int d = getDistance(b.value, target);
+            if (d < min) {
+                min = d;
+                lo = b;
+            }
+
+        }
+        return lo.value;
+
+
+    }
+
+    public static int getDistance(int value, int target) {
+        return value > target ? value - target : target - value;
+    }
+
+    static class BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value) {
+            this.value = value;
+        }
+    }
+
+}
